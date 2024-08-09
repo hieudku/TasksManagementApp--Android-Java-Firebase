98c5d787fa8bb846afa8f42453880a30d1d57f18
Huba <hieu.dcu@gmail.com>
2024-08-09 15:10:03 +1200

    Added logic for MenuActivity
    Edited MenuActivity.xml, MenuActivity.java
    Added AddTaskActivity, ViewTaskActivity classes

--- app/src/main/java/com/example/to_do_app/MenuActivity.java
+++ app/src/main/java/com/example/to_do_app/MenuActivity.java
@@ -1,6 +1,9 @@
 package com.example.to_do_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,28 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
+
+        Button addTaskButton = findViewById(R.id.add_task_button);
+        Button viewTaskButton = findViewById(R.id.view_task_button);
+
+        // Click event listener for add task button
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, AddTasksActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Click event listener for view task button
+        viewTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, ViewTaskActivity.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menu), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
