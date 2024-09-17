49a38521e23052a08b85e2ccd49e981141cc1be9
Huba <hieu.dcu@gmail.com>
2024-09-17 22:39:16 +1200

    Update: due date for tasks

--- app/src/main/java/com/example/to_do_app/Task.java
+++ app/src/main/java/com/example/to_do_app/Task.java
@@ -1,23 +1,27 @@
 package com.example.to_do_app;
 
+import com.google.firebase.Timestamp;
+
 public class Task {
     // Fields
     private String id;
     private String title;
     private String description;
+    private Timestamp dueDate;  // New field for due date
 
     // Default constructor for DataSnapshot.getValue(Task.class)
     public Task() {
     }
 
     // Parameterized Constructor
-    public Task(String id, String title, String description) {
+    public Task(String id, String title, String description, Timestamp dueDate) {
         this.id = id;
         this.title = title;
         this.description = description;
+        this.dueDate = dueDate;  // Initialize the due date
     }
 
-    // Getters and Setters for task id, title, description
+    // Getters and Setters for task id, title, description, dueDate
     public String getId() {
         return id;
     }
@@ -40,5 +44,13 @@ public class Task {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Timestamp getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Timestamp dueDate) {
+        this.dueDate = dueDate;
     }
 }
